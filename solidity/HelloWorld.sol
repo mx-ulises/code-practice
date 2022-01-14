1bd2a3badababa68ae11fc4b51c34251dfe918c5
Ulises Martinez Araiza <mx.ulises@gmail.com>
2022-01-13 20:23:48 -0800

    Solidity Excercise: https://solidity-by-example.org/hello-world/

---
+++ solidity/HelloWorld.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// Version of Solidity, in this case is 0.8.7 or greater but less than 0.9.0
+pragma solidity ^0.8.7;
+
+// Contract Name
+contract HelloWorld {
+
+    // Contract with single constant value
+    string public greet = "Hello World Ulises";
+
+}
