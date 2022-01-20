838347b034d797ac5b1369abd22ac1325c79c76b
Ulises Martinez Araiza <mx.ulises@gmail.com>
2022-01-19 23:47:15 -0800

    Solidity Excercise: https://solidity-by-example.org/immutable/

---
+++ solidity/Immutables.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Immutables {
+    
+    // Immutable variables are like constants and can be only set inside the constructor but not afterwards.
+    address public immutable ADDRESS;
+    uint public immutable VALUE;
+
+    constructor(uint _input) {
+        ADDRESS = msg.sender;
+        VALUE = _input;
+    }
+
+    function getImmutable() public view returns (uint) {
+        //VALUE = 69; <- this doesn't compile
+        return VALUE + 3;
+    } 
+}
