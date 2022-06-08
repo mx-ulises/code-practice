cc21f038f5b1b29e7745642b9b7041c43cb28511
Ulises Martinez Araiza <mx.ulises@gmail.com>
2022-06-07 23:54:04 -0700

    Improvement to problem: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/

--- python/lowest-common-ancestor-of-a-binary-search-tree.py
+++ python/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -13,3 +13,15 @@ def get_lower_common_ancestor(node, p, q):
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         return get_lower_common_ancestor(root, p, q)
+
+
+    def lowestCommonAncestor_better(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        node = root
+        while node:
+            if p.val < node.val and q.val < node.val:
+                node = node.left
+            elif node.val < p.val and node.val < q.val:
+                node = node.right
+            else:
+                return node
+        return None
