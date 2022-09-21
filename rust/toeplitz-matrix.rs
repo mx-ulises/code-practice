f2a9e0b0216729850ad215f490d24f91c901a875
Ulises Martinez Araiza <mx.ulises@gmail.com>
2022-09-20 22:52:48 -0700

    Problem: https://leetcode.com/problems/toeplitz-matrix/ (Rust Day 12/100)

---
+++ rust/toeplitz-matrix.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn is_toeplitz_matrix(matrix: Vec<Vec<i32>>) -> bool {
+        let m = matrix.len();
+        let n = matrix[0].len();
+        for i in 1..m {
+            for j in 1..n {
+                if matrix[i][j] != matrix[i - 1][j - 1] {
+                    return false;
+                }
+            }
+        }
+        return true
+    }
+}
