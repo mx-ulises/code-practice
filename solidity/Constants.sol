755aa2c73d36348ab8ec8205635c26bd35f62100
Ulises Martinez Araiza <mx.ulises@gmail.com>
2022-12-22 15:11:43 -0800

    Solidity Day 5/100: https://solidity-by-example.org/constants/

---
+++ solidity/Constants.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract Constants {
+    // Coding convention to uppercase constant variables
+    address public constant CONST_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant CONST_UINT = 640;
+}
