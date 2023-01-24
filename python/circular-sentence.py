dc3fa8c53e2196a0878051f6fafd047a44f1949a
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-01-23 20:23:59 -0800

    Problem: https://leetcode.com/problems/circular-sentence/

---
+++ python/circular-sentence.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        for i in range(1, len(sentence)):
+            if sentence[i] == ' ':
+                if sentence[i - 1] != sentence[i + 1]:
+                    return False
+        return sentence[0] == sentence[-1]
