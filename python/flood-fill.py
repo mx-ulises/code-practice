226e7ca6eb2d8e01f47466413b7b5236438f1589
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-02-25 13:53:58 -0800

    Fix to: https://leetcode.com/problems/flood-fill/

--- python/flood-fill.py
+++ python/flood-fill.py
@@ -1,22 +1,22 @@
 DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 class Solution:
-    def is_valid(image: List[List[int]], i, j, color, visited) -> bool:
+    def is_valid(image: List[List[int]], i, j, color) -> bool:
         if i < 0 or i == len(image):
             return False
         if j < 0 or j == len(image[0]):
             return False
-        return image[i][j] == color and (i, j) not in visited
+        return image[i][j] == color
 
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         old_color = image[sr][sc]
+        if color == old_color:
+            return image
         s = [(sr, sc)]
-        visited = set()
         while s:
             i, j = s.pop()
-            if Solution.is_valid(image, i, j, old_color, visited) == False:
+            if Solution.is_valid(image, i, j, old_color) == False:
                 continue
-            visited.add((i, j))
             image[i][j] = color
             for delta in DIRECTIONS:
                 s.append((i + delta[0], j + delta[1]))
