99a3efc6f7b32bc4afc2a9718bc3bc7e6e7fea48
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-03-08 20:01:06 -0800

    Problem: https://leetcode.com/problems/binary-search/

---
+++ python/binary-search.py
@@ -0,0 +1,13 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if target == nums[mid]:
+                return mid
+            elif target <= nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
