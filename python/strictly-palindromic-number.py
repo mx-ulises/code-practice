b7896dcd320b6c6d62a1cc0d3e9d3628ac4aa81c
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-04-04 22:11:52 -0700

    Add comments to: https://leetcode.com/problems/strictly-palindromic-number/

--- python/strictly-palindromic-number.py
+++ python/strictly-palindromic-number.py
@@ -1,3 +1,5 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
+        # for n = 4, n in base_2 is 100 (non palindromic)
+        # for n > 4, n in base_(n - 2) is always 12
         return False
