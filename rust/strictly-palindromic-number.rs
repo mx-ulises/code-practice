ec3d855519c461805e599396ef5c3b61bbd4ad47
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-04-04 22:08:57 -0700

    Rust Day 85/100: https://leetcode.com/problems/strictly-palindromic-number/

---
+++ rust/strictly-palindromic-number.rs
@@ -0,0 +1,7 @@
+impl Solution {
+    pub fn is_strictly_palindromic(n: i32) -> bool {
+        // For n = 4, n in base_2 is 100 (Non-palindromic)
+        // For n > 4, n in base_(n - 2) is 12 (Non-palindromic)
+        return false;
+    }
+}
