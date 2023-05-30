2f3cdb36d4cfd77110d9e57903441cd6bec660db
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-05-30 14:02:25 -0700

    Fix for https://leetcode.com/problems/maximum-binary-tree-ii/

--- python/maximum-binary-tree-ii.py
+++ python/maximum-binary-tree-ii.py
@@ -4,25 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
-    def construct_tree(node: Optional[TreeNode], val: int):
+    def construct_tree(node: Optional[TreeNode], new_node: Optional[TreeNode]):
         if node.right == None:
-            node.right = TreeNode(val)
-        elif node.right.val < val:
-            new_node = TreeNode(val)
+            node.right = new_node
+        elif node.right.val < new_node.val:
             new_node.left = node.right
             node.right = new_node
         else:
-            Solution.construct_tree(node.right, val)
+            Solution.construct_tree(node.right, new_node)
 
 
     def insertIntoMaxTree(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        new_node = TreeNode(val)
         if root == None:
-            return TreeNode(val)
+            return new_node
         if root.val < val:
-            node = TreeNode(val)
-            node.left = root
-            return node
-        Solution.construct_tree(root, val)
+            new_node.left = root
+            return new_node
+        Solution.construct_tree(root, new_node)
         return root
