69aa37d9f342733266d92081d2300e36c7e710e2
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-06-02 23:19:52 -0700

    Problem: https://leetcode.com/problems/beautiful-arrangement/

---
+++ python/beautiful-arrangement.py
@@ -0,0 +1,22 @@
+class Solution:
+    def is_valid_placement(perm_i, i, perm):
+        if perm[i]:
+            return False
+        if perm_i % i == 0 or i % perm_i == 0:
+            return True
+        return False
+
+    def build_beautiful_permutation(perm_i, perm):
+        if len(perm) == perm_i:
+            return 1
+        count = 0
+        for i in range(1, len(perm)):
+            if Solution.is_valid_placement(perm_i, i, perm):
+                perm[i] = True
+                count += Solution.build_beautiful_permutation(perm_i + 1, perm)
+                perm[i] = False
+        return count
+
+    def countArrangement(self, n: int) -> int:
+        perm = [False] * (n + 1)
+        return Solution.build_beautiful_permutation(1, perm)
