bd1baf4c4f89f5c8cfaf2344e3128f8015f34a8e
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-06-05 23:24:42 -0700

    Go Day 34/100: https://leetcode.com/problems/root-equals-sum-of-children/

---
+++ go/root-equals-sum-of-children.go
@@ -0,0 +1,12 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func checkTree(root *TreeNode) bool {
+    return root.Val == (root.Left.Val + root.Right.Val)
+}
