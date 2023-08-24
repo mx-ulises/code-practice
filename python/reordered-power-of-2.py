9c252ddc793a1f7c967aea0fba8036b0c31f45a9
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-08-24 08:03:57 -0700

    Fixing Problem: https://leetcode.com/problems/reordered-power-of-2/

--- python/reordered-power-of-2.py
+++ python/reordered-power-of-2.py
@@ -1,22 +1,21 @@
 class Solution:
-    def get_power_key(power: int):
+    def get_power_key(power: int) -> int:
         power_key = []
         while 0 < power:
             power_key.append(str(power % 10))
             power //= 10
         power_key.sort()
         power_key.reverse()
-        return "".join(power_key)
+        return int("".join(power_key))
 
-    def get_power_keys(limit: int) -> List:
+    def check_power_key(power_key: int) -> bool:
         i = 1
-        power_keys = set([])
-        while i <= limit:
-            power_keys.add(Solution.get_power_key(i))
+        while i <= power_key:
+            if Solution.get_power_key(i) == power_key:
+                return True
             i <<= 1
-        return power_keys
+        return False
 
     def reorderedPowerOf2(self, n: int) -> bool:
         power_key = Solution.get_power_key(n)
-        power_keys = Solution.get_power_keys(int(power_key))
-        return power_key in power_keys
+        return Solution.check_power_key(power_key)
