84f65cb2cfbccdbdc6fe8ead25f8b6290cd51d40
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-09-10 17:03:14 -0700

    Improvement to: https://leetcode.com/problems/minimum-cost-for-tickets/

--- python/minimum-cost-for-tickets.py
+++ python/minimum-cost-for-tickets.py
@@ -1,6 +1,5 @@
 class Solution:
-    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        costs = [(1, costs[0]), (7, costs[1]), (30, costs[2])]
+    def binary_search(days: List[int], costs: List[int]) -> int:
         memory = [0]
         for i in range(len(days)):
             memory.append(365000)
@@ -9,3 +8,21 @@ class Solution:
                 prev = bisect.bisect_left(days, ticket_start_day)
                 memory[-1] = min(memory[-1], memory[prev] + cost)
         return memory[len(days)]
+
+    def using_memory(days: List[int], costs: List[int]) -> int:
+        memory = [0] * (days[-1] + 1)
+        i = 0
+        for day in range(1, len(memory)):
+            if day < days[i]:
+                memory[day] = memory[day - 1]
+            else:
+                i += 1
+                memory[day] = 365000
+                for ticket_legth, cost in costs:
+                    ticket_start_day = max(0, day - ticket_legth)
+                    memory[day] = min(memory[day], memory[ticket_start_day] + cost)
+        return memory[-1]
+
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        costs = [(1, costs[0]), (7, costs[1]), (30, costs[2])]
+        return Solution.using_memory(days, costs)
