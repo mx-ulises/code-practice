b8319d082695350b68c901813905d361e286e7a7
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-10-18 15:53:53 -0700

    Problem: https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/

---
+++ python/final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,8 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        for i in range(len(prices)):
+            for j in range(i + 1, len(prices)):
+                if prices[j] <= prices[i]:
+                    prices[i] -= prices[j]
+                    break
+        return prices
