5fec27abc24db17ae137d8bbd0cad10d979a8716
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-10-24 22:05:41 -0700

    Problem: https://leetcode.com/problems/set-matrix-zeroes/

---
+++ python/set-matrix-zeroes.py
@@ -0,0 +1,27 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        first_column_is_zero = False
+        first_row_is_zero = False
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+                    if i == 0:
+                        first_row_is_zero = True
+                    if j == 0:
+                        first_column_is_zero = True
+        for i in range(1, len(matrix)):
+            if matrix[i][0] == 0:
+                for j in range(len(matrix[i])):
+                    matrix[i][j] = 0
+        for j in range(1, len(matrix[0])):
+            if matrix[0][j] == 0:
+                for i in range(len(matrix)):
+                    matrix[i][j] = 0
+        if first_column_is_zero:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
+        if first_row_is_zero:
+            for j in range(len(matrix[0])):
+                matrix[0][j] = 0
